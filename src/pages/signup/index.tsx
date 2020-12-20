9a41cb46884693c42fe79b6acf6dc07ce21f79b3
BismarckOliveira <bismarckoliver@hotmail.com>
2020-12-20 13:13:30 -0300

    Config Validate

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
-import logoImg from '../../assets/logo.svg';
+import * as Yup from 'yup';
 
+import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import { Container, Content, Background } from './styles';
 
 const Signup: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string()
+          .required('E-mail é obrigatorio')
+          .email('Digite um e-mail valido'),
+        password: Yup.string().min(6, 'No minimo 6 dígitos'),
+      });
+      await schema.validate(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
