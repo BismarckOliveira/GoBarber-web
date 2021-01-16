6c304911f4048b00a6e529bd179f95281370d00f
BismarckOliveira <bismarckoliver@hotmail.com>
2021-01-16 18:58:54 -0300

    adicionando rotas e animações

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-
-import SignIn from './pages/signIn';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
 import AppProvider from './hooks';
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <>
+    <Router>
       <AppProvider>
-        <SignIn />
+        <Routes />
       </AppProvider>
 
       <GlobalStyle />
-    </>
+    </Router>
   );
 };
 
--- src/pages/signup/styles.ts
+++ src/pages/signup/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackground from '../../assets/sign-up-background.png';
@@ -17,41 +17,63 @@ export const Content = styled.div`
   place-content: center;
   width: 100%;
   max-width: 700px;
+`;
 
+const appearFromRight = keyframes`
+from{
+  opacity: 0;
+  transform: translateX(50px);
+}
+to {
+  opacity: 1;
+  transform: translateX(0)
+}
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
-    padding: 20px 0;
-    text-decoration:none;
+    margin: 20px 0;
     width: 340px;
     text-align: center;
 
     h1 {
       margin-bottom: 24px;
     }
-  }
+
     a {
       color: #f4ede8;
       display: block;
       margin-top: 24px;
       text-decoration: none;
       transition: color 0.2s;
+
       &:hover {
         color: ${shade(0.2, '#f4ede8')};
       }
     }
   }
+
   > a {
-    color: #f4ede8;
+    color: #ff9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
     transition: color 0.2s;
     display: flex;
     align-items: center;
-    
+
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
+  }
+  svg {
+    margin-right: 16px;
   }
 `;
 
